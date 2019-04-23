04ba4f03166a2699b87281ef63690cc890ed0db5
Tammy Husnetdinova <tamhus31@gmail.com>
2019-04-23 16:26:24 -0600

    mongoose

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var Movie = require('./Movies');
 var Review = require('./Reviews');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
+var mongoose = require('mongoose');
 
 var app = express();
 module.exports = app; // for testing
